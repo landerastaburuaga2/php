4b09ce61504418f3b77595490370bda066df0ef6
AS3-2-05 <AS3-2-05@uni.lan>
2019-12-11 12:01:28 +0100

    qaaa

---
+++ composer/irudiak.php
@@ -0,0 +1,15 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+// import the Intervention Image Manager Class
+use Intervention\Image\ImageManagerStatic as Image;
+
+// configure with favored image driver (gd by default)
+Image::configure(array('driver' => 'imagick'));
+
+// and you are ready to go ...
+$image = Image::make('foo.jpg')->resize(300, 200);
+
+
+?>
